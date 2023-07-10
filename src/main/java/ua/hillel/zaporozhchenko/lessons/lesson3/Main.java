0e220d203598b088e779658e4d00641ff420b0db
Zaporozhchenko Bohdan <b.s.zaporozhchenko@gmail.com>
2023-07-10 19:48:33 +0300

    lesson 3 commit

---
+++ src/main/java/ua/hillel/zaporozhchenko/lessons/lesson3/Main.java
@@ -0,0 +1,8 @@
+package ua.hillel.zaporozhchenko.lessons.lesson3;
+
+public class Main {
+    public static void main(String[] args) {
+        char specSymb = '\u2713';
+        System.out.println("Lesson 3 completed" + " " + specSymb);
+    }
+}
