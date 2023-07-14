75eb22c59b1a6b3120af4c442426382a2e6f11eb
Zaporozhchenko Bohdan <b.s.zaporozhchenko@gmail.com>
2023-07-14 16:34:57 +0300

    lesson 4 commit

--- src/main/java/ua/hillel/zaporozhchenko/lessons/lesson4/Main.java
+++ src/main/java/ua/hillel/zaporozhchenko/lessons/lesson4/Main.java
@@ -2,11 +2,34 @@ package ua.hillel.zaporozhchenko.lessons.lesson4;
 
 public class Main {
     public static void main(String[] args) {
-       int a = 10;
-       int b = 5;
-       int result = ++a - b++ - a++ + ++a;
-        System.out.println("result = " + result);
-        System.out.println("a = " + a);
-        System.out.println("b = " + b);
+        int a = 5;
+        int b = 15;
+        int c = 4;
+        int volume = a * b * c;
+        int lenght = a + b + c;
+        System.out.println("Об'єм паралелепіпеда = " + volume);
+        System.out.println("Сумарна довжина сторін = " + lenght);
+
+
+
+        int leeWarrior = 13;
+        int leeArcher = 24;
+        int leeRider = 46;
+        int amountLee = 860;
+        int totalstrenghtLee = ((leeWarrior+leeRider+leeArcher) * amountLee);
+
+        int minWarrior = 9;
+        int minArcher = 35;
+        int minRider = 12;
+        double amountMin = amountLee * 1.5;
+        int totalstrenghtMin = ((minWarrior+minArcher+minRider) * (int)amountMin);
+
+        int commonpower = totalstrenghtLee + totalstrenghtMin;
+
+        System.out.println("Загальний показник атаки династії Лі = " + totalstrenghtLee);
+        System.out.println("Загальний показник атаки династії Мінь = " + totalstrenghtMin);
+        System.out.println("Загальний показник атаки обох династій = " + commonpower);
+
+
     }
 }
