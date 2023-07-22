cf7d34f317a66a09ae6d38c610c01aedd3b6e550
Zaporozhchenko Bohdan <b.s.zaporozhchenko@gmail.com>
2023-07-22 16:29:30 +0300

    guess the number lesson 6

--- src/main/java/ua/hillel/zaporozhchenko/lessons/lesson6/Main.java
+++ src/main/java/ua/hillel/zaporozhchenko/lessons/lesson6/Main.java
@@ -1,21 +1,31 @@
 package ua.hillel.zaporozhchenko.lessons.lesson6;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        int randomNum = (int) (Math.random() * 11);
         System.out.println("Guess the number from 0 to 10");
-        int randomNum = (int)(Math.random() * 11);
-        while (true) {
-            int userNum = scan.nextInt();
-            if (randomNum == userNum) {
-                System.out.println("Congratulations! You guessed the number");
-                break;
-
-            } else {
-                System.out.println("Try again");
+        Scanner scan = new Scanner(System.in);
+        int minNum = 0;
+        int maxNum = 10;
+        int attempts = 5;
+         while (attempts > 0){
+             int userNum = scan.nextInt();
+                if (userNum == randomNum) {
+                    System.out.println("Congratulations! You won!");
+                    break;
+                } else if (userNum > maxNum) {
+                    System.out.println("Enter the number from 0 to 10. Attempts: " + --attempts );
+                }  else if (userNum < minNum) {
+                    System.out.println("Enter the number from 0 to 10. Attempts: " + --attempts);
+                } else {
+                    System.out.println("Try again. Attempts: " + --attempts);
+                } if (attempts == 0) {
+                 System.out.println("You loose");
+             }
             }
-        } scan.close();
     }
 }
+
